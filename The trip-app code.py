684fad6707d079e586fea3045dc9a4e3d7732ea4
Reut Karyna <89328520+Reut-Karyna@users.noreply.github.com>
2023-05-08 00:19:37 +0800

    Create The trip-app code.py

---
+++ The trip-app code.py
@@ -0,0 +1,16 @@
+def trip_planner_welcome(name):
+  print("Welcome to tripplanner v1.0 " + name)
+trip_planner_welcome("Karyna")
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+estimate = estimated_time_rounded(2.5)   
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport="Car"):
+  origin = print("Your trip starts off in " + origin)
+  destination = print("And you are traveling to " + destination)
+  mode_of_transport = print("You will be traveling by " + mode_of_transport)
+  estimated_time = print("It will take approximately " + str(estimated_time) + " hours")
+
+destination_setup("Ukraine", "Spain", estimate, "plane") 
